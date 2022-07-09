594cde32ea9af56db3e501e2124b7ca359e229d0
Nathan Meyer <84550580+nathanwmeyer@users.noreply.github.com>
2022-07-09 15:27:34 -0700

    Update MainController.java

--- src/main/java/com/example/demo/controller/MainController.java
+++ src/main/java/com/example/demo/controller/MainController.java
@@ -17,7 +17,7 @@ import com.example.demo.model.ProductModel;
 import com.example.demo.model.UserModel;
 
 @Controller
-@RequestMapping("/")
+//@RequestMapping("/")
 public class MainController {
 
     @Autowired

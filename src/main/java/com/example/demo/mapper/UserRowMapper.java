374e2d6c2738d05047305b60338da8a211b80ed6
nathanwmeyer <84550580+nathanwmeyer@users.noreply.github.com>
2022-08-04 14:37:05 -0700

    Logging Update

--- src/main/java/com/example/demo/mapper/UserRowMapper.java
+++ src/main/java/com/example/demo/mapper/UserRowMapper.java
@@ -3,14 +3,20 @@ package com.example.demo.mapper;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.springframework.jdbc.core.RowMapper;
 
 import com.example.demo.data.entity.UserEntity;
 
 public class UserRowMapper implements RowMapper<UserEntity>{
 
+    Logger logger = LogManager.getLogger(UserRowMapper.class);
+
     @Override
     public UserEntity mapRow(ResultSet rs, int rowNum) throws SQLException {
+        logger.info("INFO: entering mapRow method");
+        logger.info("INFO: exiting mapRow method");
         return new UserEntity(rs.getLong("ID"), rs.getString("USERNAME"), rs.getString("EMAIL"));
     }
     
